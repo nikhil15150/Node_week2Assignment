d039f7b5e9d967af1c45d9aedecd8a3220348c28
nikhil <nikhil15150@iiitd.ac.in>
2020-05-04 18:31:01 +0530

    Leader rest api completed ,so second week assignment completed

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishesRouter= require('./routes/dish');
 var promotionsRouter=require('./routes/promo');
+var leaderRouter=require('./routes/leader');
 
 var app = express();
 
@@ -38,6 +39,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dishes',dishesRouter);
 app.use('/promotions',promotionsRouter);
+app.use('/leaders',leaderRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
